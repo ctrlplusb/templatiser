c527a9b565fbe81472360e676dc256de37611022
Sean Matheson <sean@ctrlplusb.com>
2018-06-28 14:32:46 +0100

    Makes the tests deterministic.

--- tests/exec.test.js
+++ tests/exec.test.js
@@ -1,12 +1,11 @@
-const path = require('path')
 const exec = require('../src/exec')
 
 describe('exec', () => {
   it('should produce the expected meta data', () => {
     const actual = exec({
-      templatesDir: path.join(__dirname, 'templates'),
-      inputDir: path.join(__dirname, 'example'),
-      outputDir: path.join(__dirname, 'output'),
+      templatesDir: 'tests/templates',
+      inputDir: 'tests/example',
+      outputDir: 'tests/output',
       allowedFiles: ['.txt'],
       preprocessSource: x => `preprocessed-${x}`,
     })
